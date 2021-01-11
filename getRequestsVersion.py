86581167da03b5e09333948c541cc77f144c53d2
Amy Xiang <yangyi1@ualberta.ca>
2021-01-11 15:48:48 -0700

    added references

--- getRequestsVersion.py
+++ getRequestsVersion.py
@@ -1,11 +1,12 @@
 import requests
 
+# Referenced alko (November 24, 2013) on StackOverflow: https://stackoverflow.com/questions/20180543/how-to-check-version-of-python-modules
 print("Python Requests Module Version: " + requests.__version__ )
 
 print("--- \n")
 
+# Referenced FlyingV (May 18, 2020) on StackOverflow: https://stackoverflow.com/questions/23049767/parsing-http-response-in-python 
 response = requests.get('https://raw.githubusercontent.com/amy-xiang/CMPUT404-lab1/master/getRequestsVersion.py')
-
 data = response.content.decode('utf-8')
 
 print(data)
